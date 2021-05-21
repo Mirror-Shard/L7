f2b0517eab4279692330a016c658363359ba807e
Mirror-Shard <Inncvation@outlook.com>
2021-05-21 09:11:20 +0300

    fixes

--- individual/ind 1.py
+++ individual/ind 1.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python3
 import sys
 
-# Ввести список одной строкой
-print("Введите 10 целочисленных элементов в массив через пробел:")
-a = tuple(map(int, input().split()))
 
-if len(a) != 10:
-    print("Неверный размер списка", file=sys.stderr)
-    exit(1)
+if __name__ == '__main__':
+    # Ввести список одной строкой
+    print("Введите 10 целочисленных элементов в массив через пробел:")
+    a = tuple(map(int, input().split()))
 
-sum = 0
-q = 0
+    if len(a) != 10:
+        print("Неверный размер списка", file=sys.stderr)
+        exit(1)
 
-for i in enumerate(a, 0):
-    if i[1] % 2 == 0:
-        sum += i[1]
-        q += 1
+    sum = 0
+    q = 0
 
-print(f"Сумма чётных элементов равна: {sum}, всего чётных элементов: {q}")
+    for i in enumerate(a, 0):
+        if i[1] % 2 == 0:
+            sum += i[1]
+            q += 1
+
+    print(f"Сумма чётных элементов равна: {sum}, всего чётных элементов: {q}")
